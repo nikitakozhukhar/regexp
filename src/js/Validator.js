aa584abd9cc2853c821e273a0e0ab916e5281764
NikitaKx9 <nikitakx9@gmail.com>
2023-06-07 17:02:57 +0300

    Validator.js change

--- src/js/Validator.js
+++ src/js/Validator.js
@@ -3,11 +3,16 @@ class Validator {
 		this.name = name;
 	}
 	validateUsername(userName) {
-		this.userName = userName;
-		const re = /(^[а-я])[-_][0-9]/i;
+		
+		const re = /[a-z][^0-9]/i;
+		// const re = /\d/    [-_][0-9]
 		console.log(re.test(userName))
 		return re.test(userName);
 		//Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9);
 
 	}
 }
+
+let validator = new Validator();
+
+validator.validateUsername('Nikita111')
