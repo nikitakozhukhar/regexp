a111f35ce0da33b2987fd3250240f010af1bb5ed
NikitaKx9 <nikitakx9@gmail.com>
2023-06-11 16:00:00 +0300

    Validator.js remove app.js changed test done

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,46 @@
 import sum from '../basic';
+import Validator from '../app';
 
 test('should sum', () => {
   const result = sum([1, 2, 3]);
 
   expect(result).toBe(6);
+});
+
+const validator = new Validator();
+
+test('Допустимы только латинские буквы', () => {
+  const result = validator.validateUsername('Евлампия');
+
+  expect(result).toBe(false);
+});
+
+test('Имя не может содерать более трех цифр', () => {
+  const result = validator.validateUsername('Marco1234Polo');
+
+  expect(result).toBe(false);
+});
+
+test('Имя не может начинаться с цифр', () => {
+  const result = validator.validateUsername('111MarcoPolo');
+
+  expect(result).toBe(false);
+});
+test('Имя не может заканчиваться цифрами', () => {
+  const result = validator.validateUsername('MarcoPolo11');
+
+  expect(result).toBe(false);
+});
+
+test('Имя не может начинаться с символов "-" и "_"', () => {
+  const result = validator.validateUsername('-MarcoPolo');
+  const result2 = validator.validateUsername('_MarcoPolo');
+  expect(result).toBe(false);
+  expect(result2).toBe(false);
+});
+test('Имя не может заканчиваться символами "-" и "_"', () => {
+  const result = validator.validateUsername('MarcoPolo-');
+  const result2 = validator.validateUsername('MarcoPolo_');
+  expect(result).toBe(false);
+  expect(result2).toBe(false);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,14 @@ import sum from './basic';
 console.log('worked');
 
 console.log(sum([1, 2]));
+
+export default class Validator {
+  constructor(name) {
+    this.name = name;
+  }
+
+  validateUsername(userName) {
+    const re = /^(?!(.*\d){4})[^\-_][^\d][a-z\-_0-9]+[^\-_0-9]+$/i;
+    return re.test(userName);
+  }
+}
